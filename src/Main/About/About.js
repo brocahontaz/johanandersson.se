88bcfbf32aa0d48bc78cbec842715079a20b2caa
Johan Andersson <ja222um@student.lnu.se>
2021-01-13 14:58:08 +0100

    Added skeleton for About section.

---
+++ src/Main/About/About.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './About.css'
+
+const About = () =>
+  <div className='About'>
+    <div className='Wrapper'>
+      <div className='Section'>
+        <h2>About</h2>
+        <p className='AboutParagraph'>
+          Passionate about automation and scaling.
+        </p>
+        <p className='AboutParagraph'>
+          Studying Software Development and Operations, bachelor's degree as a software developer with an extra focus on the DevOps mindset- both development and operations of software.
+        </p>
+        <p className='AboutParagraph'>
+          Thrives very well in full stack, from server and infrastructure, backend, all the way to frontend. 
+          Thesis project completed in the spring of 2020 dealt with migration of infrastructure to, and implementation of, a full-scale Kubernetes cluster in collaboration with Sensative AB. 
+        </p>
+      </div>
+      <div className='Section'>
+        <h3>Skills</h3>
+      </div>
+      <div className='Section'>
+        <h3>Experience</h3>
+      </div>
+      <div className='Section'>
+        <h3>Education</h3>
+      </div>
+    </div>
+  </div>
+
+export default About
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
+import About from './About'
 
 import './Main.css'
 
@@ -8,6 +9,7 @@ const Main = () =>
   <div className='Main'>
     <Switch>
         <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
       </Switch>
   </div>
 
